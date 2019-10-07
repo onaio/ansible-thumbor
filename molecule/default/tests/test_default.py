9456c32b8679362f939024f4e15833ff2834333b
Morris Mukiri <morrismukiri@gmail.com>
2019-10-07 23:54:45 +0300

    Add molecule tests
    Signed-off-by: Morris Mukiri <morrismukiri@gmail.com>

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,22 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']
+).get_hosts('all')
+
+
+def test_hosts_file(host):
+    f = host.file('/etc/hosts')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
+
+
+def test_thumbor_service(host):
+    thumbor = host.service("thumbor")
+
+    assert thumbor.is_running
+    assert thumbor.is_enabled
